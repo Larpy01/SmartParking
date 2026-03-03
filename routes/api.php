fb81c70c52bf7924ee68a4eea2f71ce1646042bb
rmasudog536110-prog <r.masudog.536110@umindanao.edu.ph>
2026-03-04 04:23:55 +0800

    asdstwywaq

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,17 @@
 <?php
 
-use App\Http\Controllers\Api\{
+/**use App\Http\Controllers\Api\{
     ParkingLocationController as ApiParkingLocationController,
     ParkingSlotController as ApiParkingSlotController,
     ReservationController as ApiReservationController
-};
+}; */
+
 
 use Illuminate\Http\Request;
 use App\Models\ParkingSlot;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
+/**Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/parking-locations',
         [ApiParkingLocationController::class, 'index']);
@@ -20,7 +21,8 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/reservations',
         [ApiReservationController::class, 'store']);
-});
+}); */
+
 
 Route::get('/slot/{id}/{token}', function ($id, $token) {
 
