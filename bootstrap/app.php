7ea414fe1e53efae1c1a6b0dad9b0dfb2d2b23f4
rmasudog536110-prog <r.masudog.536110@umindanao.edu.ph>
2026-03-04 02:56:51 +0800

    bosit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\URL;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,12 +12,18 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
-
-    
+    })
+    ->booting(function () {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+    })
+    ->create();
